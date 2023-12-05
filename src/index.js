388f6d8595fea924c0d1882b8d370fdcd2c07d9c
Ashley <ashley.precios@student.moringaschool.com>
2023-12-05 09:47:39 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter ,Routes, Route } from "react-router-dom"
 
+
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
 function Home() {
   return (
     <div>
@@ -9,8 +35,13 @@ function Home() {
   );
 }
 
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
